41ff4a8389d7fa8fc0cbabf3f98300abdd15c7d3
Claudinei Perboni <cperbony@gmail.com>
2019-01-02 23:44:55 -0200

    Usando biblioteca JS (rails-assets.org)

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,5 +1,7 @@
 class WelcomeController < ApplicationController
   def index
+    cookies[:curso] = "Curso de Ruby on Rails 5.x"
+    session[:curso] = "Curso de Ruby on Rails 5.x [SESSION]"
     @meu_nome = params[:nome]
     @curso = params[:curso]
   end
